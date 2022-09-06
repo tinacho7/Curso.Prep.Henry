8d96bfcc29f57d1d5c5a2ddc758ea4651356cb74
tinacho7 <acabralrobledo@gmail.com>
2022-09-06 16:45:39 -0300

    correccion avance

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -148,7 +148,7 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (numero > 0) {
+  if (numero >= 0) {
     return "Es positivo" ;
   }
   return "Es negativo" ;
@@ -185,7 +185,7 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  return Math.pow(lado,2) ;
+  return lado * 4 ;
 }
 
 
